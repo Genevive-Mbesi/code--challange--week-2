68a29ee9de359bf28279372c9ab60148eaca8635
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-06-27 14:21:01 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+//Fetch Request
+
 function getData(){
     fetch('http://localhost:3000/characters')
     .then (res => res.json())
     .then(animalData => animalData.forEach(animals =>renderOneCharacter(animals)) )
 }
 
+// DOM render function
+
 function renderOneCharacter(animal){
+
+//Build animal
+
     let myList = document.createElement('li')
     myList.className = 'myList'
     myList.innerHTML =`
@@ -15,17 +22,28 @@ function renderOneCharacter(animal){
     $<span class ="vote-count">${animal.votes}</span> Voted
     </p>
     <p>${animal.description}</p>
+    <div class ="buttons">
+    <button> Voted </button>
+    </div>
     
     `
-    console.log(myList)
+//Add animal to DOM
+    
     document.querySelector('#characters-list').appendChild(myList)
+
     
 }
+
+// Get Data and Render our animals to DOM
  
 function initialize() {
     getData()
 }
 initialize()
+
+
+
+
 
 
 
